5cbfdebc58929436947e572afe147ade62fc200b
Dan Naeem <dan.naeem95@gmail.com>
2018-06-19 13:31:04 -0400

    create MainApp.js

---
+++ sono-vivace-app/src/components/MainApp.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class MainApp extends Component {
+  render() {
+    return(
+      <div>
+        <p>This is the main app</p>
+      </div>
+    );
+  }
+}
+
+export default MainApp;
