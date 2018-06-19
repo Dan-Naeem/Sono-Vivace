ce0e91ab87baa394162b030b982aa77cccccf272
Dan Naeem <dan.naeem95@gmail.com>
2018-06-19 14:45:05 -0400

    setup musicplayer skeleton

--- sono-vivace-app/src/components/MainApp.js
+++ sono-vivace-app/src/components/MainApp.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react';
 import {styles} from '../styles/styles';
 
+//import card
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+
 class MainApp extends Component {
   render() {
     return(
       <div style={styles.mainApp}>
         <div style={styles.left}>
-          <p>left</p>
+          <div style={styles.musicPlayerContainer}>
+            <Card style={styles.musicPlayer}>
+              <h3>Music Player</h3>
+              <p>song goes here</p>
+            </Card>
+          </div>
+          <div style={styles.playlist}>
+          </div>
         </div>
         <div style={styles.right}>
           <p>Right</p>
--- sono-vivace-app/src/styles/styles.js
+++ sono-vivace-app/src/styles/styles.js
@@ -1,9 +1,6 @@
 const styles = {
   mainApp: {
     border: '5px red solid',
-    width: '100%',
-    minWidth: '100%',
-    maxWidth: '100%',
     height: '100%',
     minHeight: '100%',
     maxHeight: '100%',
@@ -14,15 +11,32 @@ const styles = {
     justifyContent: 'center',
   },
   left: {
-    border: '5px yellow solid',
     width: '40%',
     overFlow: 'auto',
+    padding: '.5em'
   },
   right: {
-    border: '5px green solid',
     width: '60%',
     overFlow: 'auto',
-  }
+    padding: '.5em'
+  },
+  musicPlayerContainer: {
+    border: '5px blue solid',
+    width: '100%',
+    paddingTop: '100%',
+    position: 'relative',
+  },
+  musicPlayer: {
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    bottom: '0',
+    right: '0',
+  },
+  playlist: {
+    border: '5px yellow solid',
+    height: 'auto',
+  },
 };
 
 export {
