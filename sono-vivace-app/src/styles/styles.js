09f8ded7ba61586541abb177fadfbc04444397c1
Dan Naeem <dan.naeem95@gmail.com>
2018-06-19 13:38:52 -0400

    create styles.js

---
+++ sono-vivace-app/src/styles/styles.js
@@ -0,0 +1,14 @@
+const styles = {
+  mainApp: {
+    border: '5px red solid',
+    margin: '.5em',
+    padding: '.5em',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  }
+};
+
+export {
+  styles
+};
