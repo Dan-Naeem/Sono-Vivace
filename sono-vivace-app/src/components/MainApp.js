2a470298d84c220e72792e5cf825658af7123af2
Dan Naeem <dan.naeem95@gmail.com>
2018-06-19 13:39:03 -0400

    import styles

--- sono-vivace-app/src/components/MainApp.js
+++ sono-vivace-app/src/components/MainApp.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
+import {styles} from '../styles/styles';
 
 class MainApp extends Component {
   render() {
     return(
-      <div>
+      <div style={styles.mainApp}>
         <p>This is the main app</p>
       </div>
     );
