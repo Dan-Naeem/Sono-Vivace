3dc7c0cbc73c24b2bb4550082a0dc352cf9c6e19
Dan Naeem <dan.naeem95@gmail.com>
2018-06-19 11:47:48 -0400

    import ButtonAppBar

--- sono-vivace-app/src/App.js
+++ sono-vivace-app/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import ButtonAppBar from './components/ButtonAppBar';
+
 class App extends Component {
   render() {
     return (
       <div className="App" >
-        <p>yerp</p>
+        <ButtonAppBar />
       </div>
     );
   }
